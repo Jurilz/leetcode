b53f50cab4336b1f6deea07940392218f59ff614
julozowo <j.lozowoj@micromata.de>
2024-04-02 13:28:09 +0200

    formatting

--- src/main/kotlin/removeElement/RemoveElement.kt
+++ src/main/kotlin/removeElement/RemoveElement.kt
@@ -4,9 +4,7 @@ class RemoveElement {
 
     companion object {
         fun removeElement(nums: IntArray, `val`: Int): Int {
-
             var index = 0
-
             for (i in 0..(nums.size - 1)) {
                 if (nums[i] != `val`) {
                     nums[index] = nums[i]
@@ -14,7 +12,6 @@ class RemoveElement {
                     index++
                 }
             }
-
             return index
         }
     }
